01bcb80c04d0df48f2ee1d7add08ebfb092ef5a0
daniloff79 <danilo@asaidera.com.br>
2026-03-18 20:46:12 -0300

    ajuste no nome

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,12 +2,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'app.lovable.a3ca81e5b32d4e9f90fe83ea2061697e',
-  appName: 'posture-buddy-bot',
+  appName: 'Posture Notification',
   webDir: 'dist',
-  server: {
+  /*server: {
     url: 'https://a3ca81e5-b32d-4e9f-90fe-83ea2061697e.lovableproject.com?forceHideBadge=true',
     cleartext: true,
-  },
+  },*/
 };
 
 export default config;
